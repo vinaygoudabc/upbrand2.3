91873831e465ed8a06c52e06745faffca7bbd534
vinaygouda <=>
2023-04-03 21:38:22 +0530

    new test.java is committed

---
+++ test.java
@@ -0,0 +1,8 @@
+public class GitHub{
+	public ststic void main(String args[]){
+		System.out.println("using this file to see the git diff");
+		System.out.println("This is new file to see the git diff");
+		
+		
+	}
+}
